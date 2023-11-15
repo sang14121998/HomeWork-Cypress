e80a54ac94e113765ce917e694a60bea39a702b6
SangTT <trantansang1412@gmail.com>
2023-11-15 19:24:52 +0700

    upload login

---
+++ cypress/e2e/login-demo.cy.js
@@ -0,0 +1,20 @@
+describe("login", () => {
+    it("TC1 - register get element by CSS ID selector" , () => {
+        cy.visit("https://demoqa.com/text-box");
+        cy.get("#userName").type("Tran Tan Sang");
+        cy.get("#userEmail").type("trantansang1412@gmail.com");
+        cy.get("#currentAddress").type("Ha Noi city");
+        cy.get("#permanentAddress").type("Nam Dinh city");
+        cy.get("#submit").click();
+    })
+
+    it ("TC2 - register get element by Relative Xpath", () => {
+        cy.visit("https://demoqa.com/text-box");
+        cy.xpath("//input[@id='userName']").type("Tran Tan Sang");
+        cy.xpath("//input[@id='userEmail']").type("trantansang1412@gmail.com");
+        cy.xpath("//textarea[@id='currentAddress']").type("Ha Noi 01 city");
+        cy.xpath("//textarea[@id='permanentAddress']").type("Nam Dinh 01 city");
+        cy.xpath("//button[@id='submit']").click();
+
+    })
+})
