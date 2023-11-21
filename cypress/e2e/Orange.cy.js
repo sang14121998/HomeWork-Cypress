dd20db4601a9d22d6cbf81c90fcf82e1dbf39aec
SangTT <trantansang1412@gmail.com>
2023-11-21 22:10:10 +0700

    update

--- cypress/e2e/Orange.cy.js
+++ cypress/e2e/Orange.cy.js
@@ -2,13 +2,13 @@ describe("Login and Edit info Account", () => {
     let fName = "Sang";
     let mName = "Tan";
     let lName = "Tran";
-    let idName = 14449009;
+    let idName = 7;
     let fNameEdit = "John";
     let mNameEdit= "Wick";
     let lNameEdit = "Parker";
-    let idNameEdit = 47244;
+    let idNameEdit = 2040;
     let nickName = "SangTT";
-    let otherId = 8447;
+    let otherId = 444;
 
 
     it("TC1-edit info is successfully", () => {
@@ -27,23 +27,21 @@ describe("Login and Edit info Account", () => {
 
         // check data inputed
         cy.xpath("(//i[@class='oxd-icon bi-caret-down-fill oxd-select-text--arrow'])[1]").should("exist");
-        cy.xpath("//input[@placeholder='First name']").should("be.enabled").and("have.value", fName);
-        cy.xpath("//input[@placeholder='Middle name']").should("be.enabled").and("have.value", mName);
+        cy.xpath("//input[@placeholder='First Name']").should("be.enabled").and("have.value", fName);
+        cy.xpath("//input[@placeholder='Middle Name']").should("be.enabled").and("have.value", mName);
         cy.xpath("//input[@placeholder='Last Name']").should("be.enabled").and("have.value", lName);
         cy.xpath("(//input[@class='oxd-input oxd-input--active'])[3]").should("be.enabled").and("have.value", idName);
    
         // update info 
-        cy.xpath("//input[@placeholder='First name']").clear().type(fNameEdit);
-        cy.xpath("//input[@placeholder='Middle name']").clear().type(mNameEdit);
+        cy.xpath("//input[@placeholder='First Name']").clear().type(fNameEdit);
+        cy.xpath("//input[@placeholder='Middle Name']").clear().type(mNameEdit);
         cy.xpath("//input[@placeholder='Last Name']").clear().type(lNameEdit);
         cy.xpath("(//input[@class='oxd-input oxd-input--active'])[1]").should("be.enabled").clear().type(nickName);       
         cy.xpath("(//input[@class='oxd-input oxd-input--active'])[3]").clear().type(idNameEdit);
         cy.xpath("(//input)[7]").should("be.enabled").clear().type(otherId); 
         cy.xpath("(//input[@placeholder='yyyy-mm-dd'])[1]").should("be.enabled").clear().type("2030-12-25").click();
 
-        
-        cy.get(".oxd-icon bi-caret-down-fill oxd-select-text--arrow")
-          .select("Vietnames");
+
 
 
 
