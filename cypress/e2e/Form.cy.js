cd89816f965da8c4021cc0b87d7fcace11a19902
SangTT <trantansang1412@gmail.com>
2023-11-21 21:42:59 +0700

    edit TC form

--- cypress/e2e/Form.cy.js
+++ cypress/e2e/Form.cy.js
@@ -4,40 +4,40 @@ describe("Test form", ()=> {
     // Lấy giờ hiện tại
     const currentDate = new Date();
   
-    // it("TC1 - submit form - verify required input", () =>{
-    //     cy.visit("https://demoqa.com/automation-practice-form");
-    //     cy.xpath("//input[@id='firstName']").should("be.enabled").type("Alice");
-    //     cy.xpath("//input[@id='lastName']").should("be.enabled").type("Pham");
-    //     cy.xpath("//label[normalize-space()='Male']").should("not.be.checked");
-    //     cy.xpath("//label[normalize-space()='Female']").should("not.be.checked");
-    //     cy.xpath("//label[normalize-space()='Other']").should("not.be.checked");
-    //     cy.xpath("//label[normalize-space()='Female']").click();
-    //     cy.xpath("//input[@id='userNumber']").should("be.enabled").type("0123456789");         
-    //     cy.xpath("//button[@id='submit']").should("be.enabled").click({force: true});
+    it("TC1 - submit form - verify required input", () =>{
+        cy.visit("https://demoqa.com/automation-practice-form");
+        cy.xpath("//input[@id='firstName']").should("be.enabled").type("Alice");
+        cy.xpath("//input[@id='lastName']").should("be.enabled").type("Pham");
+        cy.xpath("//label[normalize-space()='Male']").should("not.be.checked");
+        cy.xpath("//label[normalize-space()='Female']").should("not.be.checked");
+        cy.xpath("//label[normalize-space()='Other']").should("not.be.checked");
+        cy.xpath("//label[normalize-space()='Female']").click();
+        cy.xpath("//input[@id='userNumber']").should("be.enabled").type("0123456789");         
+        cy.xpath("//button[@id='submit']").should("be.enabled").click({force: true});
 
-    //     // check visible popup
-    //     cy.xpath("(//div[@class='modal-header'])[1]").should("be.visible");
+        // check visible popup
+        cy.xpath("(//div[@class='modal-header'])[1]").should("be.visible");
 
-    //     // check value of table 
-    //     cy.get("tbody tr:nth-child(1) td:nth-child(2)").should("have.text", "Alice Pham");
-    //     cy.get("tbody tr:nth-child(2) td:nth-child(2)").should("have.text", "");
-    //     cy.get("tbody tr:nth-child(3) td:nth-child(2)").should("have.text", "Female");
-    //     cy.get("tbody tr:nth-child(4) td:nth-child(2)").should("have.text","0123456789");
+        // check value of table 
+        cy.get("tbody tr:nth-child(1) td:nth-child(2)").should("have.text", "Alice Pham");
+        cy.get("tbody tr:nth-child(2) td:nth-child(2)").should("have.text", "");
+        cy.get("tbody tr:nth-child(3) td:nth-child(2)").should("have.text", "Female");
+        cy.get("tbody tr:nth-child(4) td:nth-child(2)").should("have.text","0123456789");
 
-    //     // Định dạng thời gian với DD Month YYYY
-    //     const formattedDate = format(currentDate, 'dd MMMM,yyyy');
-    //     // Kiểm tra nội dung hiển thị trong phần tử   
-    //     cy.get("tbody tr:nth-child(5) td:nth-child(2)").should("have.text", formattedDate);
+        // Định dạng thời gian với DD Month YYYY
+        const formattedDate = format(currentDate, 'dd MMMM,yyyy');
+        // Kiểm tra nội dung hiển thị trong phần tử   
+        cy.get("tbody tr:nth-child(5) td:nth-child(2)").should("have.text", formattedDate);
 
-    //     cy.get("tbody tr:nth-child(6) td:nth-child(2)").should("have.text", "");
-    //     cy.get("tbody tr:nth-child(7) td:nth-child(2)").should("have.text", "");
-    //     cy.get("tbody tr:nth-child(8) td:nth-child(2)").should("have.text", "");
-    //     cy.get("tbody tr:nth-child(9) td:nth-child(2)").should("have.text", "");
-    //     cy.get("tbody tr:nth-child(10) td:nth-child(2)").should("have.text", "");
+        cy.get("tbody tr:nth-child(6) td:nth-child(2)").should("have.text", "");
+        cy.get("tbody tr:nth-child(7) td:nth-child(2)").should("have.text", "");
+        cy.get("tbody tr:nth-child(8) td:nth-child(2)").should("have.text", "");
+        cy.get("tbody tr:nth-child(9) td:nth-child(2)").should("have.text", "");
+        cy.get("tbody tr:nth-child(10) td:nth-child(2)").should("have.text", "");
 
-    //     cy.get("#closeLargeModal").should("be.enabled").click({force: true});
-    //     cy.xpath("(//div[@class='modal-header'])[1]").should("not.exist");
-    // })
+        cy.get("#closeLargeModal").should("be.enabled").click({force: true});
+        cy.xpath("(//div[@class='modal-header'])[1]").should("not.exist");
+    })
     
     it("TC2 -Check data of all file after close popup", () => {
         cy.visit("https://demoqa.com/automation-practice-form");
--- cypress/e2e/Orange.cy.js
+++ cypress/e2e/Orange.cy.js
@@ -2,24 +2,55 @@ describe("Login and Edit info Account", () => {
     let fName = "Sang";
     let mName = "Tan";
     let lName = "Tran";
+    let idName = 14449009;
+    let fNameEdit = "John";
+    let mNameEdit= "Wick";
+    let lNameEdit = "Parker";
+    let idNameEdit = 47244;
+    let nickName = "SangTT";
+    let otherId = 8447;
+
+
     it("TC1-edit info is successfully", () => {
         cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-        cy.xpath("//input[@placeholder='username']").type("Admin");
-        cy.xpath("//input[@placeholder='password']").type("admin123");
+        cy.xpath("//input[@placeholder='Username']").type("Admin");
+        cy.xpath("//input[@placeholder='Password']").type("admin123");
         cy.get("button[type='submit']").click();
         cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/pim/addEmployee");
-        cy.xpath("//input[@placeholder='First name']").should("be.enabled").type(fName);
-        cy.xpath("//input[@placeholder='Middle name']").should("be.enabled").type(mName);
+        cy.xpath("//input[@placeholder='First Name']").should("be.enabled").type(fName);
+        cy.get("input[placeholder='Middle Name']").should("be.enabled").type(mName);
         cy.xpath("//input[@placeholder='Last Name']").should("be.enabled").type(lName);
-        cy.xpath("(//input[@class='oxd-input oxd-input--active'])[2]").should("be.enabled").type("1412");
+        cy.xpath("(//input[@class='oxd-input oxd-input--active'])[2]").should("be.enabled").clear().type(idName);
         cy.xpath("//button[normalize-space()='Save']").should("be.enabled").click();
 
         cy.url().should("include", "viewPersonalDetails/empNumber/");
 
-        // check datat inputed
+        // check data inputed
+        cy.xpath("(//i[@class='oxd-icon bi-caret-down-fill oxd-select-text--arrow'])[1]").should("exist");
         cy.xpath("//input[@placeholder='First name']").should("be.enabled").and("have.value", fName);
         cy.xpath("//input[@placeholder='Middle name']").should("be.enabled").and("have.value", mName);
         cy.xpath("//input[@placeholder='Last Name']").should("be.enabled").and("have.value", lName);
+        cy.xpath("(//input[@class='oxd-input oxd-input--active'])[3]").should("be.enabled").and("have.value", idName);
+   
+        // update info 
+        cy.xpath("//input[@placeholder='First name']").clear().type(fNameEdit);
+        cy.xpath("//input[@placeholder='Middle name']").clear().type(mNameEdit);
+        cy.xpath("//input[@placeholder='Last Name']").clear().type(lNameEdit);
+        cy.xpath("(//input[@class='oxd-input oxd-input--active'])[1]").should("be.enabled").clear().type(nickName);       
+        cy.xpath("(//input[@class='oxd-input oxd-input--active'])[3]").clear().type(idNameEdit);
+        cy.xpath("(//input)[7]").should("be.enabled").clear().type(otherId); 
+        cy.xpath("(//input[@placeholder='yyyy-mm-dd'])[1]").should("be.enabled").clear().type("2030-12-25").click();
+
+        
+        cy.get(".oxd-icon bi-caret-down-fill oxd-select-text--arrow")
+          .select("Vietnames");
+
+
+
+
+
+
+
 
     })
 })
