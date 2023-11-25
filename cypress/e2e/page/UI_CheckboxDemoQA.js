d0ee03d9364d61557d612cda91d9d83841748bb2
SangTT <trantansang1412@gmail.com>
2023-11-26 06:52:04 +0700

    update testcase TableCheckboxQA

--- cypress/e2e/page/UI_CheckboxDemoQA.js
+++ cypress/e2e/page/UI_CheckboxDemoQA.js
@@ -1,6 +1,12 @@
 const URL = "https://demoqa.com/checkbox";
 const ALL_LIST = ".check-box-tree-wrapper";
-const HOME_LIST_ARROW = ".rct-icon rct-icon-expand-open";
+const BTN_HOME_LIST = "(//button[@title='Toggle'])[1]";
+const BTN_DOWNLOADS_LIST = "(//button[@title='Toggle'])[4]";
+const CHECKBOX_HOME = "#tree-node-home";
+const CHECKBOX_WORLD_FILE = "#tree-node-wordFile";
+const STATUS_HALF_CHECK = ".rct-icon rct-icon-half-check";
+const STATUS_CHECK_ALL = ".rct-icon rct-icon-check";
+const STATUS_UNCHECK = ".rct-icon rct-icon-uncheck";
 const DOWNLOADS_LIST ="#tree-node-downloads";
 const WORD_FILE_DOC = "//span[contains(text(),'Word File.doc')]";
 
@@ -19,8 +25,34 @@ export default class CheckboxList {
 
     get getHomeListClick() {
         return (
-            cy.get(HOME_LIST_ARROW).should("be.enabled").click()
+            cy.xpath(BTN_HOME_LIST).should("be.enabled").click({force: true})
         
         )
     }
+
+    get getDownloadListClick() {
+        return (
+            cy.xpath(BTN_DOWNLOADS_LIST).should("be.enabled").click({force: true})
+        
+        )
+    }
+
+    get getCheckboxHome() {
+        return CHECKBOX_HOME;
+    }
+
+    get getCheckboxWordFileClick() {
+        return (
+            cy.get(CHECKBOX_WORLD_FILE)
+              .should("be.enabled")
+              .and("not.be.checked")
+              .click({force: true})
+        )
+    }
+
+    get getStatusHaftCheck() {
+        return STATUS_HALF_CHECK;
+    }
+
+
 }
